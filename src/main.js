9bdf55fd531fe70c2824ebc7e66054b297b8bf98
Gruvix <timmy.burton@hotmail.com>
2023-10-16 21:27:34 -0300

    add start button

--- src/main.js
+++ src/main.js
@@ -7,3 +7,7 @@
 //second click - compare cards, remove or hide cards
 //after second click, if cards are 0, end game
 
+document.querySelector("#start").addEventListener("click", () => {
+    document.querySelector("#start").classList.add("hidden");
+    document.querySelector("#game").classList.remove("hidden");
+})
