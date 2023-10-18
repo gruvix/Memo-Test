4d18ce7a16bbeaf6b01264a033b9780545735d80
Gruvix <timmy.burton@hotmail.com>
2023-10-18 17:12:08 -0300

    test now visits page before each check

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,23 @@
-describe('visits page and checks title', () => {
-  it('The title is "MemoTest"', () => {
+context('MemoTest', () => {
+  
+  before(() => {
     cy.visit('/')
-    cy.get('h1').contains('MemoTest')
   })
-})
 
-describe('plays the game', () => {
-  const CARDS_LENGHT = 12
-  it('finishes de game', () => {
-    cy.visit('/')
-    cy.get('#start').click()
-    cy.get('#game').should('be.visible')
-    cy.get('.card').should('have.length', CARDS_LENGHT)
+
+  describe('visits page and checks title', () => {
+    it('The title is "MemoTest"', () => {
+      cy.get('h1').contains('MemoTest')
+    })
   })
-})
 
+  describe('plays the game', () => {
+    const CARDS_TOTAL_LENGHT = 12
+    it('finishes de game', () => {
+      cy.get('#start').click()
+      cy.get('#game').should('be.visible')
+      cy.get('.card').should('have.length', CARDS_TOTAL_LENGHT)
+    })
+  })
+
+})
