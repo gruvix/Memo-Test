4b7268cf877398ec29d61a1faea4d43c03ab6f03
Gruvix <timmy.burton@hotmail.com>
2023-10-18 17:53:54 -0300

    tests removed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,20 +4,22 @@ context('MemoTest', () => {
     cy.visit('/')
   })
 
-
-  describe('visits page and checks title', () => {
-    it('The title is "MemoTest"', () => {
-      cy.get('h1').contains('MemoTest')
-    })
-  })
-
   describe('plays the game', () => {
-    const CARDS_TOTAL_LENGHT = 12
-    it('finishes de game', () => {
-      cy.get('#start').click()
-      cy.get('#game').should('be.visible')
-      cy.get('.card').should('have.length', CARDS_TOTAL_LENGHT)
-    })
-  })
+    //const CARDS_TOTAL_LENGHT = 12
+    // it('finds the title is "MemoTest"', () => {
+    //   cy.get('h1').contains('MemoTest')
+    // })
+    // it('starts the game', () => {
+    //   cy.get('#start').click()
+    // })
 
+    // it('has 12 cards', () => {
+    //   cy.get('.card').should('have.length', CARDS_TOTAL_LENGHT)
+    // })
+    // it('shows the game container', () => {
+    //   cy.get('#game').should('be.visible')
+    // })
+
+
+  })
 })
