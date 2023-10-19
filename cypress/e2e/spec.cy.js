a740aca8443af357ad47dc8378c6c50b84629db6
Gruvix <timmy.burton@hotmail.com>
2023-10-19 13:27:08 -0300

    tests for starting the game

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,10 +3,6 @@ context('MemoTest', () => {
 
 
   describe('plays the game', () => {
-    // it('starts the game', () => {
-    //   cy.get('#start').click()
-    // })
-
     // it('has 12 cards', () => {
     //   cy.get('.card').should('have.length', CARDS_TOTAL_LENGHT)
     // })
@@ -20,6 +16,10 @@ context('MemoTest', () => {
     it('finds the title is "MemoTest"', () => {
       cy.get('h1').contains('MemoTest')
     });
+    it('starts the game', () => {
+      cy.get('#start').click()
+    });
+
 
 
   })
