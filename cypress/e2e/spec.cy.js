df2d9ea39f9b34da27b685db6abe6d6fa8693a73
Gruvix <timmy.burton@hotmail.com>
2023-10-19 13:26:58 -0300

    tests for finding the title

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,8 @@
 context('MemoTest', () => {
   
-  before(() => {
-    cy.visit('/')
-  })
+
 
   describe('plays the game', () => {
-    //const CARDS_TOTAL_LENGHT = 12
-    // it('finds the title is "MemoTest"', () => {
-    //   cy.get('h1').contains('MemoTest')
-    // })
     // it('starts the game', () => {
     //   cy.get('#start').click()
     // })
@@ -19,6 +13,13 @@ context('MemoTest', () => {
     // it('shows the game container', () => {
     //   cy.get('#game').should('be.visible')
     // })
+    beforeEach(() => {
+      cy.visit('/')
+    });
+
+    it('finds the title is "MemoTest"', () => {
+      cy.get('h1').contains('MemoTest')
+    });
 
 
   })
